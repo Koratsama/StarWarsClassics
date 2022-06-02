966c4c57d7435bd4a1ca27f5b99371af75aeb5a5
Koratsama <sliston21@gmail.com>
2022-06-01 20:28:41 -0500

    menu unit tests

--- menu/menu.go
+++ menu/menu.go
@@ -19,43 +19,39 @@ Parameters: None
 func Start() bool {
 
 	var shutdown bool = false
-	for !shutdown {
-		var choice string
+	var choice string
 
-		fmt.Println("\n1. Sabacc\n2. Corellian Spike\n3. Coruscant Shift\n4. Quit" +
-			"\nPlease select a game to launch:")
+	fmt.Println("\n1. Sabacc\n2. Corellian Spike\n3. Coruscant Shift\n4. Quit" +
+		"\nPlease select a game to launch:")
 
-		in := bufio.NewReader(os.Stdin)
-		choice, err := in.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-			return true
-		}
-		choice = strings.Split(choice, "\n")[0]
-		//TODO: don't use Scan, use "bufio", "os"?
-		//fmt.Scanf("%v\n", &choice)
+	in := bufio.NewReader(os.Stdin)
+	choice, err := in.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+		return true
+	}
+	choice = strings.Split(choice, "\n")[0] //get user input
 
-		switch choice {
-		case "1", "Sabacc", "sabacc":
-			fmt.Println("\nThank you for choosing Sabacc!" +
-				"\n... unfortunately this game is not available at this time." +
-				"\nPlease choose another.")
-		case "2", "Corellian Spike", "corellian spike":
-			fmt.Println("\nThank you for choosing Corellian Spike Sabacc!")
-			sabacc.Start()
-			fmt.Println("\nThank you for playing!...")
-		case "3", "Coruscant Shift", "coruscant shift":
-			fmt.Println("\nThank you for choosing Coruscant Shift Sabacc!" +
-				"\n... unfortunately this game is not available at this time." +
-				"\nPlease choose another.")
-		case "4", "q", "Q", "Quit", "quit":
-			fmt.Println("May the force be with you...")
-			time.Sleep(1 * time.Second)
-			fmt.Println("always")
-			shutdown = true
-		default:
-			fmt.Printf("\nInvalid option: %s\nPlease choose again.\n", choice)
-		}
+	switch choice {
+	case "1", "Sabacc", "sabacc":
+		fmt.Println("\nThank you for choosing Sabacc!" +
+			"\n... unfortunately this game is not available at this time." +
+			"\nPlease choose another.")
+	case "2", "Corellian Spike", "corellian spike":
+		fmt.Println("\nThank you for choosing Corellian Spike Sabacc!")
+		sabacc.Start()
+		fmt.Println("\nThank you for playing!...")
+	case "3", "Coruscant Shift", "coruscant shift":
+		fmt.Println("\nThank you for choosing Coruscant Shift Sabacc!" +
+			"\n... unfortunately this game is not available at this time." +
+			"\nPlease choose another.")
+	case "4", "q", "Q", "Quit", "quit":
+		fmt.Println("May the force be with you...")
+		time.Sleep(1 * time.Second)
+		fmt.Println("always")
+		shutdown = true
+	default:
+		fmt.Printf("\nInvalid option: %s\nPlease choose again.\n", choice)
 	}
 	return shutdown
 }
--- menu/menu_test.go
+++ menu/menu_test.go
@@ -10,10 +10,112 @@ import (
 	"github.com/Koratsama/StarWarsClassics/menu"
 )
 
-func TestMenuStart(t *testing.T) {
+func TestMenuSabacc(t *testing.T) {
 
-	//TODO: actually test user input for the menu
-	fmt.Printf("test Okay!")
+	content := []byte("Sabacc\n")
+	tmpfile, err := ioutil.TempFile("", "tempfile")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer os.Remove(tmpfile.Name()) // clean up
+
+	if _, err := tmpfile.Write(content); err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := tmpfile.Seek(0, 0); err != nil {
+		log.Fatal(err)
+	}
+
+	oldStdin := os.Stdin
+	defer func() { os.Stdin = oldStdin }() // Restore original Stdin
+
+	os.Stdin = tmpfile
+
+	//run menu
+	var shutdown bool = menu.Start()
+
+	if shutdown == true {
+		fmt.Println("Error reading player input.")
+		t.Fail()
+	}
+
+	if err := tmpfile.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestMenuCorellianSpike(t *testing.T) {
+
+	content := []byte("Corellian Spike\n")
+	tmpfile, err := ioutil.TempFile("", "tempfile")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer os.Remove(tmpfile.Name()) // clean up
+
+	if _, err := tmpfile.Write(content); err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := tmpfile.Seek(0, 0); err != nil {
+		log.Fatal(err)
+	}
+
+	oldStdin := os.Stdin
+	defer func() { os.Stdin = oldStdin }() // Restore original Stdin
+
+	os.Stdin = tmpfile
+
+	//run menu
+	var shutdown bool = menu.Start()
+
+	if shutdown == true {
+		fmt.Println("Error reading player input.")
+		t.Fail()
+	}
+
+	if err := tmpfile.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestMenuCoruscantShift(t *testing.T) {
+
+	content := []byte("Coruscant Shift\n")
+	tmpfile, err := ioutil.TempFile("", "tempfile")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer os.Remove(tmpfile.Name()) // clean up
+
+	if _, err := tmpfile.Write(content); err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := tmpfile.Seek(0, 0); err != nil {
+		log.Fatal(err)
+	}
+
+	oldStdin := os.Stdin
+	defer func() { os.Stdin = oldStdin }() // Restore original Stdin
+
+	os.Stdin = tmpfile
+
+	//run menu
+	var shutdown bool = menu.Start()
+
+	if shutdown == true {
+		fmt.Println("Error reading player input.")
+		t.Fail()
+	}
+
+	if err := tmpfile.Close(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func TestMenuQuitPath(t *testing.T) {
@@ -42,6 +144,41 @@ func TestMenuQuitPath(t *testing.T) {
 	var shutdown bool = menu.Start()
 
 	if shutdown != true {
+		fmt.Println("Error reading player input.")
+		t.Fail()
+	}
+
+	if err := tmpfile.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestMenuInvalidInput(t *testing.T) {
+	content := []byte("Dejarik\n")
+	tmpfile, err := ioutil.TempFile("", "tempfile")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer os.Remove(tmpfile.Name()) // clean up
+
+	if _, err := tmpfile.Write(content); err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := tmpfile.Seek(0, 0); err != nil {
+		log.Fatal(err)
+	}
+
+	oldStdin := os.Stdin
+	defer func() { os.Stdin = oldStdin }() // Restore original Stdin
+
+	os.Stdin = tmpfile
+
+	//run menu
+	var shutdown bool = menu.Start()
+
+	if shutdown == true {
 		fmt.Println("Error reading player input.")
 		t.Fail()
 	}
